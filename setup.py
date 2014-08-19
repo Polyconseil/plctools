0b4f9043656677255f3cfda34f0cdcabdc372e73
Benoit Calvez <benoit.calvez@polyconseil.fr>
2014-08-19 13:20:57 +0200

    Add setup.py and version.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from setuptools import find_packages, setup
+
+_version_file = open(os.path.join(os.path.dirname(__file__), 'version.py'))
+VERSION = re.compile(r"^VERSION = '(.*?)'", re.S).match(_version_file.read()).group(1)
+
+setup(
+    name="plctools",
+    version=VERSION,
+    author="Polyconseil dev team",
+    author_email="autolib-dev@polyconseil.fr",
+    description=("Collection of tools to manipulate a PLC."),
+    license="BSD",
+    keywords="modbus",
+    url="https://github.com/polyconseil/plctools",
+    packages=find_packages(),
+    long_description="",
+    install_requires=open('requirements.txt').read(),
+    entry_points = {
+        'console_scripts': [
+            'plc_dump = plc_dump:main',
+            'plc_load = plc_load:main',
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python 3",
+    ],
+    test_suite='',
+    include_package_data=True,
+)
